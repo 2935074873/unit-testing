ee93fb3cbbaf2a2970cc3e27cd63af9658c0f186
xu.ting <xu.ting@mgm-iot.comxu.ting@mgm-iot.com>
2020-07-02 14:15:48 +0800

    test

--- test/add.test.js
+++ test/add.test.js
@@ -1,11 +1,15 @@
 const add    =  require('../add'),
       expect =  require('chai').expect;
 describe('加法函数的单元测试',function(){ //测试套件
-    it('0 + 0 = 0',function() {
+    it('0 + 0 = 0',function() {    //测试用例
         expect(add(0,0,)).to.be.equal(0)
     })
 
-    it('1 + 0 = 0',function() {
-        expect(add(1,0,)).to.be.equal(1)
+    it('1 + 4 = 5',function() {
+        expect(add(1,4,)).to.be.equal(5)
+    })
+
+    it('a+b=NaN',function(){
+        expect(add('a','b')).to.be.NaN
     })
 })
